52f2b0f1a2ccd559d10f37eb92c10863ecdaf55b
JeongTaek Han <smart8612@gmail.com>
2021-05-03 21:42:00 +0900

    update TrainCustomModel.py

--- TrainCustomModel.py
+++ TrainCustomModel.py
@@ -11,34 +11,35 @@ from sklearn.metrics import accuracy_score
 import pickle
 
 
-modelFileName = "KMU.pkl"
+modelFileName = "mymy.pkl"
 
 if __name__ == '__main__':
     df = pd.read_csv('coords.csv')
-    # sad_query = df[df['class'] == 'Sad']
-    # happy_query = df[df['class'] == 'Happy']
-    # victory_query = df[df['class'] == 'Victorious']
 
-    x = df.drop('class', axis=1)  # features
-    y = df['class']  # target value
+    x = df.drop('class', axis=1)    # features: Pose, Face
+    y = df['class']                 # target value
 
+    # Partition Dataset for Train Model
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1234)
 
+    # Configure Model Pipeline for inference
     pipelines = {
         'lr': make_pipeline(StandardScaler(), LogisticRegression()),
-        'rc': make_pipeline(StandardScaler(), RidgeClassifier()),
-        'rf': make_pipeline(StandardScaler(), RandomForestClassifier()),
-        'gb': make_pipeline(StandardScaler(), GradientBoostingClassifier()),
     }
 
+    # Train Model
     fit_models = {}
     for algo, pipeline in pipelines.items():
         model = pipeline.fit(x_train, y_train)
         fit_models[algo] = model
 
+    # Measure inferenced data Accuracy
     for algo, model in fit_models.items():
         inferenced_y = model.predict(x_test)
         print(algo, accuracy_score(y_test, inferenced_y))
 
+    # Save model fitting result as binary data
     with open(modelFileName, 'wb') as f:
         pickle.dump(fit_models['lr'], f)
+
+    exit()
